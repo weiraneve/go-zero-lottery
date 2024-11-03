c6e740579e894cded8a749434795feb945e430c1
weiraneve <907221539@qq.com>
2024-11-03 14:50:41 +0800

    feat: update error definition

--- internal/model/vars.go
+++ internal/model/vars.go
@@ -1,5 +1,12 @@
 package model
 
-import "github.com/zeromicro/go-zero/core/stores/sqlx"
+import (
+	"errors"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
-var ErrNotFound = sqlx.ErrNotFound
+var (
+	ErrNotFound        = sqlx.ErrNotFound
+	ErrInvalidObjectId = errors.New("invalid objectId")
+	ErrNotUpdate       = errors.New("no rows for update")
+)
