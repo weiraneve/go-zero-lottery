2f4aa48633428207efa6f4bd20e54bc3a3e380e7
weiraneve <907221539@qq.com>
2024-12-21 08:48:57 +0800

    feat: add team get

--- internal/logic/log_logic.go
+++ internal/logic/log_logic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"lottery/internal/model"
 	"lottery/internal/svc"
 	"lottery/internal/types"
 
@@ -24,6 +26,18 @@ func NewLogLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LogLogic {
 }
 
 func (l *LogLogic) Log(req *types.LogRequest) (resp *types.LogResponse, err error) {
+	_, err = l.svcCtx.TeamModel.FindOneByEncryptCode(l.ctx, req.EncryptCode)
+	if err != nil {
+		if errors.Is(err, model.ErrNotFound) {
+			logx.Errorw("team not found",
+				logx.Field("encrypt_code", req.EncryptCode),
+				logx.Field("error", err),
+			)
+			return nil, errors.New("team not found, please check your encrypt code")
+		} else {
+			return nil, errors.New("internal server error")
+		}
+	}
 
 	return &types.LogResponse{}, nil
 }
