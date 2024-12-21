99783d07224dcb9600ad34d797f02aed3230364e
weiraneve <907221539@qq.com>
2024-12-22 00:32:25 +0800

    refactor: remove log model useless method

--- internal/model/log_model.go
+++ internal/model/log_model.go
@@ -1,9 +1,6 @@
 package model
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
@@ -21,25 +18,6 @@ type (
 		*defaultLogModel
 	}
 )
-
-func (m *customLogModel) FindAllByEncryptCode(ctx context.Context, encryptCode string) ([]*Log, error) {
-	if m == nil || m.conn == nil {
-		return nil, errors.New("model or database connection is nil")
-	}
-
-	query := fmt.Sprintf("SELECT %s FROM %s WHERE `encrypt_code` = ?", logRows, m.table)
-	var logs []*Log
-	err := m.conn.QueryRowsCtx(ctx, &logs, query, encryptCode)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(logs) == 0 {
-		return nil, ErrNotFound
-	}
-
-	return logs, nil
-}
 
 // NewLogModel returns a model for the database table.
 func NewLogModel(conn sqlx.SqlConn) LogModel {
